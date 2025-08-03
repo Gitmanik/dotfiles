54357c5fe7560a49388b272c02965488259556d5
Pawel Reich <37516105+Gitmanik@users.noreply.github.com>
2025-08-04 00:24:06 +0200

    nvim: [WIP] Roslyn fix

--- .config/nvim/lua/goat/plugins/roslyn.lua
+++ .config/nvim/lua/goat/plugins/roslyn.lua
@@ -1,9 +1,15 @@
 return {
-    "seblyng/roslyn.nvim",
+    "benjiwolff/roslyn.nvim",
+    branch = "bugfix-fix-buffer-highlighting-after-initialization",
     ft = "cs",
     ---@module 'roslyn.config'
     ---@type RoslynNvimConfig
     opts = {
-        -- your configuration comes here; leave empty for default settings
+        filewatching = "roslyn",
+        broad_search = true,
+        choose_target = "Renode_NET.sln",
+        ignore_target = function(target)
+            return string.match(target, "Renode.sln") ~= nil
+        end
     },
 }
