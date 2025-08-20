ec8510746b5d79c29abdea693927f016937bafc6
Pawel Reich <preich@internships.antmicro.com>
2025-08-20 09:29:32 +0200

    tmux: Open notes 6 days back

---
+++ .config/fish/functions/open_notes.fish
@@ -0,0 +1,9 @@
+function open_notes --description "Opens notes"
+    for i in (seq 0 6)
+        set -l name (date -d"-$i days" +%d_%m_%y)
+        set -f -a files "$name.md"
+    end
+    pushd "$HOME/notes"
+    nvim $files
+    popd
+end
