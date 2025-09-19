1abca13cf60202106e5e14ae3f6e0dbae821cfc5
Pawel Reich <preich@internships.antmicro.com>
2025-09-19 14:50:21 +0200

    fish: renode_analyzer 2.0

--- .config/fish/functions/renode_analyzer.fish
+++ .config/fish/functions/renode_analyzer.fish
@@ -1,12 +1,14 @@
 function renode_analyzer --description "analyzer"
     while true
-        set port (ss -lntp | grep dotnet | grep -oP '(?<=0.0.0.0:)\d+' | head -n 1)
+        set port (ss -lntp | grep dotnet | grep -oP '(?<=0.0.0.0:)\d+' | grep -oP '[0-9]{5}' | head -n 1)
 
         if test -n "$port"
             echo "Connecting to localhost:$port"
             telnet localhost $port
         end
 
+        clear
+        figlet -w (tput cols) -c renode_analyzer
         sleep 0.5
     end
 end
